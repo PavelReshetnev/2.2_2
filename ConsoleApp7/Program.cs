371e5963fb766f47522cf9f3ecf78f4cea9a335b
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 11:24:17 +0700

    Initial commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp7;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Train> trains = new List<Train>(); 
+        trains.Add(new Train(69, "Tomsk", "16:25")); 
+        trains.Add(new Train(23, "Tomsk", "8:00")); 
+        
+        int vizov = Convert.ToInt32(Console.ReadLine()); 
+        string otvet = ""; 
+        foreach (Train schitala in trains) { 
+            if (schitala.GetNumber == vizov){
+                otvet = schitala.traininfo();
+                break;
+            }
+        }
+
+        if (otvet != ""){
+            Console.WriteLine(otvet);
+        } else {
+            Console.WriteLine("Такого поезда не существует"); 
+        }
+    }
+}
---
+++ ConsoleApp7/Train.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp7;
+
+public class Train
+{
+    public int trainNum;
+    public string point;
+    public string departTime;
+    public Train(int trainNum, string point, string departTime) {
+        this.trainNum = trainNum;
+        this.point = point;
+        this.departTime = departTime;
+    }
+    public int GetNumber { 
+        get{
+            return trainNum;
+        }
+    }
+
+    public string traininfo()
+    {
+        return ("Номер :" + trainNum + "; След.Остановка: " + point + "; Время отправленипя: " + departTime);
+    }
+}
